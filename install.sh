680a4445ed713841ad3fc528462e6183300cfd91
Steve Blaurock <sblaurock@gmail.com>
2014-12-23 02:33:18 -0500

    More sane install logic.

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+shopt -s dotglob nullglob
+
 for i in * ; do
-  if [ -d "$i" ]; then
-    mv "${HOME}/.${i}" "${HOME}/.${i}.bak"
-    cp -R $i "${HOME}/.${i}"
+  if [[ $i != '.git'* ]] && [[ $i == .* ]] ; then
+    mv "${HOME}/${i}" "${HOME}/${i}.bak"
+    cp -R $i "${HOME}/${i}"
   fi
 done
 
