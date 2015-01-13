3e77acad6d6d35b45ab5e0fefcab0841a1057912
Steve Blaurock <sblaurock@gmail.com>
2015-01-13 01:17:45 -0500

    Added Prezto configuration, removed remaining traces of Fish.

--- install.sh
+++ install.sh
@@ -7,14 +7,6 @@ REPO_PREZTO='https://github.com/sorin-ionescu/prezto.git'
 REPO_POWERLINE_FONTS='https://github.com/powerline/fonts.git'
 REPO_VUNDLE='https://github.com/gmarik/Vundle.vim.git'
 
-# Copy config files to home directory.
-for i in * ; do
-  if [[ $i != '.git' ]] && [[ $i != '.gitignore' ]] && [[ $i == .* ]] ; then
-    rm -rf "${HOME}/${i}"
-    cp -R $i "${HOME}/${i}"
-  fi
-done
-
 # Resolve dependency: Zsh
 if [ ! -f $ZSH ] ; then
   if [[ "$OSTYPE" = "darwin"* ]] && type brew > /dev/null ; then
@@ -36,6 +28,7 @@ if [ ! -d "${HOME}/.zprezto" ] ; then
   git clone --recursive $REPO_PREZTO "${HOME}/.zprezto"
   rm "${HOME}/.zprezto/runcoms/README.md"
   zsh -c 'setopt EXTENDED_GLOB; for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/* ; do ; ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}" ; done'
+  git pull && git submodule update --init --recursive
 fi
 
 # Resolve dependency: Powerline fonts
@@ -45,6 +38,14 @@ if [ ! -f "${HOME}/Library/Fonts/Anonymice Powerline.ttf" ] ; then
   ./install.sh
   rm -rf "${HOME}/.powerline-fonts"
 fi
+
+# Copy config files to home directory.
+for i in * ; do
+  if [[ $i != '.git' ]] && [[ $i != '.gitignore' ]] && [[ $i == .* ]] ; then
+    rm -rf "${HOME}/${i}"
+    cp -R $i "${HOME}/${i}"
+  fi
+done
 
 # Resolve dependency: Vundle
 if [ ! -f "${HOME}/.vim/bundle/Vundle.vim" ] ; then
