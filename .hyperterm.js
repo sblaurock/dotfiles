2b742a916862b6e0b81b485cdf43b6d3429f043d
Steve Blaurock <sblaurock@gmail.com>
2016-07-18 13:11:50 -0400

    Update Hypeterm colors and font settings.

--- .hyperterm.js
+++ .hyperterm.js
@@ -1,7 +1,7 @@
 module.exports = {
   config: {
     fontSize: 13,
-    fontFamily: '"Hack", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: '"Source Code Pro for Powerline", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
     cursorColor: '#d7d7af',
     foregroundColor: '#d7d7af',
     backgroundColor: '#1c1c1c',
@@ -10,24 +10,24 @@ module.exports = {
     termCSS: '',
     padding: '15px 25px',
     colors: [
-      '#1c1c1c', // black (background color)
-      '#dd6964', // red (normal)
-      '#bdc38f', // green (normal)
-      '#ffd080', // yellow (normal)
-      '#56595c', // blue (normal)
-      '#fa9aff', // magenta (normal)
-      '#88afff', // cyan (normal)
-      '#f1f1f0', // grey (normal)
-      '#666666', // black (bright)
-      '#f58783', // red (bright)
-      '#aafba2', // green (bright)
-      '#ffe89c', // yellow (bright)
-      '#6ec5f9', // blue (bright)
-      '#ffb2ff', // magenta (bright)
-      '#b7f0ff', // cyan (bright)
-      '#ffffff'// white (foreground color)
+      '#1c1c1c',
+      '#ae5c51',
+      '#bdc38f',
+      '#d6af8a',
+      '#56595c',
+      '#ae594b',
+      '#878787',
+      '#f1f1f0',
+      '#666666',
+      '#e49063',
+      '#d9e0a2',
+      '#fed68c',
+      '#88afaf',
+      '#ae5c51',
+      '#88afaf',
+      '#ffffff'
     ]
   },
-  plugins: [],
+  plugins: ['hyperterm-blink'],
   localPlugins: []
 };
