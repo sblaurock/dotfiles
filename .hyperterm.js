0722d5e312c9800636d5c383694fdffca86e863e
Steve Blaurock <sblaurock@gmail.com>
2016-07-18 12:10:18 -0400

    Add Hyperterm configuration.

---
+++ .hyperterm.js
@@ -0,0 +1,33 @@
+module.exports = {
+  config: {
+    fontSize: 13,
+    fontFamily: '"Hack", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    cursorColor: '#d7d7af',
+    foregroundColor: '#d7d7af',
+    backgroundColor: '#1c1c1c',
+    borderColor: '#d7d7af',
+    css: '',
+    termCSS: '',
+    padding: '15px 25px',
+    colors: [
+      '#1c1c1c', // black (background color)
+      '#dd6964', // red (normal)
+      '#bdc38f', // green (normal)
+      '#ffd080', // yellow (normal)
+      '#56595c', // blue (normal)
+      '#fa9aff', // magenta (normal)
+      '#88afff', // cyan (normal)
+      '#f1f1f0', // grey (normal)
+      '#666666', // black (bright)
+      '#f58783', // red (bright)
+      '#aafba2', // green (bright)
+      '#ffe89c', // yellow (bright)
+      '#6ec5f9', // blue (bright)
+      '#ffb2ff', // magenta (bright)
+      '#b7f0ff', // cyan (bright)
+      '#ffffff'// white (foreground color)
+    ]
+  },
+  plugins: [],
+  localPlugins: []
+};
